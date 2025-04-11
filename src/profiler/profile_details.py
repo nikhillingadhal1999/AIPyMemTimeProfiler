39064e2c1a2c548b8dada0b78f9d5727d333c2b6
nikhillingadhal1999 <nikhillingadhal1999@gmail.com>
2025-04-12 00:17:15 +0530

    added features mem_growth_rss_kb,returned_size,cpu_time_ms

--- src/profiler/profile_details.py
+++ src/profiler/profile_details.py
@@ -1,24 +1,34 @@
 import sys
 import time
 import tracemalloc
-
 import json
 from pathlib import Path
-import runpy
 import os
+import gc
+from pympler import asizeof
+import psutil
+import io
+from line_profiler import LineProfiler
+import re
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.insert(0, ROOT_DIR)
-from src.helpers.helper import check_event, check_external_functions, get_all_args,is_user_code,detect_mem_leak
-from src.config.config import file_path,dir_path
+from src.helpers.helper import check_event, check_external_functions, get_all_args, is_user_code, detect_mem_leak
+from src.config.config import file_path, dir_path
 
 class Profiler:
-    def __init__(self, root_path, leak_threshold_kb=100):
+    def __init__(self, root_path, leak_threshold_kb=3000):
         self.root_path = Path(root_path).resolve()
         self.records = {}
         self.call_stack = {}
         self.leak_threshold_kb = leak_threshold_kb
+        self.proc = psutil.Process(os.getpid())
         tracemalloc.start()
+
+        self.line_profiler = LineProfiler()
+        self.line_profiled_functions = set()
+        self.line_profiled_output = {}
+        self.enable_line_profiling = True
 
     def profile_func(self, frame, event, arg):
         if check_event(event):
@@ -30,33 +40,61 @@ class Profiler:
         if check_external_functions(func_name):
             return self.profile_func
 
-        if not is_user_code(frame,self.root_path):
+        if not is_user_code(frame, self.root_path):
             return self.profile_func
 
         key = (func_name, code.co_filename, frame.f_lineno)
 
-        if not is_user_code(frame,self.root_path):
-            return self.profile_func
-
         if event == "call":
+            time.sleep(0.2)
+            print("Clearing mem...")
+            gc.collect()
+
             all_args = get_all_args(frame)
+            cpu_start = time.process_time()
+            wall_start = time.perf_counter()
+            start_snapshot = tracemalloc.take_snapshot()
+            mem_start = self.proc.memory_info().rss
+
             self.call_stack[frame] = {
-                "start_time": time.perf_counter(),
-                "start_snapshot": tracemalloc.take_snapshot(),
+                "start_time": wall_start,
+                "start_cpu": cpu_start,
+                "start_snapshot": start_snapshot,
                 "args": all_args,
+                "mem_start": mem_start
             }
+
             print(f"[CALL] {func_name} at {code.co_filename}")
+
         elif event == "return" and frame in self.call_stack:
+            code_obj = frame.f_code
+
+            if self.enable_line_profiling and code_obj not in self.line_profiled_functions:
+                func = frame.f_globals.get(code_obj.co_name)
+                if callable(func):
+                    self.line_profiler.add_function(func)
+                    self.line_profiled_functions.add(code_obj)
+
             call_info = self.call_stack.pop(frame)
             end_time = time.perf_counter()
+            end_cpu = time.process_time()
+            mem_end = self.proc.memory_info().rss
+
             duration = end_time - call_info["start_time"]
+            cpu_time = end_cpu - call_info["start_cpu"]
+
+            time.sleep(0.02)
+            print("Clearing mem...")
+            gc.collect()
             end_snapshot = tracemalloc.take_snapshot()
             memory_diff = end_snapshot.compare_to(call_info["start_snapshot"], "lineno")
             total_mem = sum([stat.size_diff for stat in memory_diff])
             max_mem = max([stat.size_diff for stat in memory_diff], default=0)
+            mem_growth = mem_end - call_info["mem_start"]
 
-            # Collect high memory diff lines from user code
-            leak_candidates = detect_mem_leak(memory_diff,self.leak_threshold_kb,self.root_path)
+            returned_size = asizeof.asizeof(arg)
+            returned_enough = abs(total_mem - returned_size) / 1000
+            leak_candidates = detect_mem_leak(memory_diff, self.leak_threshold_kb, self.root_path)
 
             record = self.records.get(
                 key,
@@ -65,22 +103,26 @@ class Profiler:
                     "file": code.co_filename,
                     "line": frame.f_lineno,
                     "max_time": 0,
+                    "cpu_time": 0,
                     "max_mem": 0,
-                    "mem_not_freed": 0,
+                    "mem_growth_rss": 0,
                     "args": call_info["args"],
                     "possible_memory_leak": None,
+                    "note": []
                 },
             )
 
             record["max_time"] = max(record["max_time"], duration)
+            record["cpu_time"] = max(record["cpu_time"], cpu_time)
             record["max_mem"] = max(record["max_mem"], max_mem)
             record["max_time_ms"] = round(record["max_time"] * 1000, 3)
+            record["cpu_time_ms"] = round(record["cpu_time"] * 1000, 3)
             record["max_mem_kb"] = round(record["max_mem"] / 1024, 3)
-            record["mem_not_freed"] += total_mem
-            record["mem_not_freed_kb"] = round(record["mem_not_freed"] / 1024, 3)
+            record["mem_growth_rss_kb"] = round(mem_growth / 1024, 3)
+            record["returned_size"] = returned_size
 
-            if leak_candidates:
-                record["possible_memory_leak"] = leak_candidates
+            if (self.leak_threshold_kb * 1000) < returned_size:
+                record["note"].append("Obj return size is huge. Please check.")
 
             self.records[key] = record
 
